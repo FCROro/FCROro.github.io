42eb3f935ccd2d101fa5b9d01a421c354accaffa
SpartacusDev <dev.spartacus@gmail.com>
2026-04-24 13:10:00 +0300

    Updated the website structure to make it easier on editing

--- generate_html.py
+++ generate_html.py
@@ -2,6 +2,10 @@ import json
 import os
 import config
 from typing import TypedDict, List
+from jinja2 import Environment, FileSystemLoader
+
+TEMPLATE_DIR = os.path.join(os.getcwd(), config.TEMPLATES_PATH)
+
 
 class Creator(TypedDict):
     name: str
@@ -16,140 +20,41 @@ class Character(TypedDict):
     description: str
     creators: List[Creator]
     appearances: List[str]
+    endpoint: str
 
-marvel_folder = os.path.join(os.getcwd(), config.CHARACTERS_DATA_PATH)
-marvel_characters = [os.path.join(marvel_folder, file_name) for file_name in os.listdir(marvel_folder)]
-links = []
-PADDING = "\n\t\t"
 
-footer = f"""
-    <footer id="page-footer">
-        <a href="{config.DISCORD_SERVER}">FCRO Discord Server</a>
-    </footer>"""
+def load_characters() -> List[Character]:
+    marvel_folder = os.path.join(os.getcwd(), config.CHARACTERS_DATA_PATH)
+    marvel_characters = [os.path.join(marvel_folder, file_name) for file_name in os.listdir(marvel_folder)]
 
-def generate_characters(marvel_characters, links, footer):
+    characters: List[Character] = []
     for character in marvel_characters:
         with open(character, "r") as f:
             character_data: Character = json.loads(f.read())
-        
+
         filename = character.split("/")[-1].replace(".json", ".html")
 
         marvel_characters_path = os.path.join(os.getcwd(), config.OUTPUT_PATH, config.CHARACTERS_URL_PATH)
         if not os.path.exists(marvel_characters_path):
             os.mkdir(marvel_characters_path)
         
-        filename = os.path.join(marvel_characters_path, filename)
+        character_data["endpoint"] = f"{config.CHARACTERS_URL_PATH}/{filename}"
+        characters.append(character_data)
+    return characters
 
-        with open(filename, "w") as f:
-            f.write(f"""<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../styles.css">
-        <title>{character_data['name']}</title>
-    </head>
-    <body>
-        <ul id='navbar'>
-            <li><a href="../index.html">Home</a></li>
-            <li><a href="../about.html">About</a></li>
-        </ul>
-        <h1>{character_data['name']}</h1>
-        <h2>Earth {character_data['earth']}</h2>
-        <img id='character-picture' src='{character_data['imageURL']}' alt='{character_data['imageDescription']}' />
-        <h2>Also known as:</h2>
-        <ul id='aka-list'>
-            {PADDING.join(['<li class="aka-item">' + alias + '</li>' for alias in character_data['aliases']])}
-        </ul>
-
-        <h3>Created By:</h3>
-        <ul>
-            {PADDING.join(['<li>' + creator['name'] + ': ' + creator['title'] + '</li>' for creator in character_data['creators']])}
-        </ul>
-        <h3>Description:</h3>
-        <p>{character_data['description']}</p>
-
-        <div class="header-container">
-        <h3>Appearances:</h3>
-        <select name="Order" id="order-changer">
-            <option value="newfirst">Newest first</option>
-            <option value="oldfirst">Oldest first</option>
-        </select>
-        </div>
-        <ul id='appearances'>
-            {PADDING.join(['<li>' + appearance + '</li>' for appearance in character_data['appearances']])}
-        </ul>
-
-        {footer}
-
-        <script src='character.js'></script>
-    </body>
-    </html>
-    """)
-        
-        links.append(f"<li><a href='{config.CHARACTERS_URL_PATH + '/' + filename.split('/')[-1]}'>{character_data['name'] + ' (' + ', '.join(character_data['aliases']) + ')'}</a></li>")
-
-def generate_index(links, footer):
-    filename = os.path.join(os.getcwd(), config.OUTPUT_PATH, "index.html")
-
-    with open(filename, "w") as f:
-        f.write(f"""<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="styles.css">
-        <title>FCRO</title>
-    </head>
-    <body>
-        <ul id='navbar'>
-            <li><a href="index.html">Home</a></li>
-            <li><a href="about.html">About</a></li>
-        </ul>
-        <h1>Welcome to the FCRO website!</h1>
-        <p>Brief description of the website</p>
-        
-        <h3>List of characters</h3>
-        <ul>
-            {PADDING.join(links)}
-        </ul>
-
-        {footer}
-    </body>
-    </html>""")
-    
-def generate_about(footer):
-    filename = os.path.join(os.getcwd(), config.OUTPUT_PATH, "about.html")
-    with open(filename, "w") as f:
-        f.write(f"""<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="styles.css">
-        <title>About</title>
-    </head>
-    <body>
-        <ul id='navbar'>
-            <li><a href="index.html">Home</a></li>
-            <li><a href="about.html">About</a></li>
-        </ul>
-        <h1>About us</h1>
-        <p>Brief description</p>
-        <h3>Sonar</h3>
-        <p>Brief description</p>
-        <h3>Ace Coronet</h3>
-        <p>Brief description</p>
-        
-        {footer}
-    </body>
-    </html>
-    """)
-    
 if __name__ == "__main__":
-    if not os.path.exists(os.path.join(os.getcwd(), config.OUTPUT_PATH)):
-        os.mkdir(os.path.join(os.getcwd(), config.OUTPUT_PATH));
-    
-    generate_characters(marvel_characters, links, footer)
-    generate_index(links, footer)
-    generate_about(footer)
+    env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
+    index = env.get_template("index.jinja")
+    about = env.get_template("about.jinja")
+    character = env.get_template("character.jinja")
+    characters = load_characters()
+
+    with open(os.path.join(os.getcwd(), config.OUTPUT_PATH, "index.html"), "w") as f:
+        f.write(index.render(characters=characters))
+
+    with open(os.path.join(os.getcwd(), config.OUTPUT_PATH, "about.html"), "w") as f:
+        f.write(about.render())
+
+    for character_data in characters:
+        with open(os.path.join(os.getcwd(), config.OUTPUT_PATH, character_data["endpoint"]), "w") as f:
+            f.write(character.render(character=character_data))
